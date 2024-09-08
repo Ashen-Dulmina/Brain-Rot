af0cc56200e0e0da8711573e046f2bc64bb4a1ba
Ashen Dulmina <ashendulminadodangoda@gmail.com>
2024-09-08 12:17:40 +0530

    Added the CLI tool "BrainMake"

---
+++ CLI/BrainMake.py
@@ -0,0 +1,15 @@
+#This is the official CLI tool for Brain-Rot
+#/credits: @Ashen-Dulmina
+
+import requests
+import base64
+import sys
+import os
+
+
+#############################
+#     InitializeProjects    #
+#############################
+
+def InitializeProject(NAME : str): #Establishes a project
+  print("leaving the rest foryou")
